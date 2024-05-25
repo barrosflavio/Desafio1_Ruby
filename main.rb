96bd5a557d1dfc6ed14d06acc32f57bcdd657735
barrosflavio <flavi011@outlook.com>
2024-05-25 10:10:11 -0300

    Desafio 1 ruby DIO

---
+++ main.rb
@@ -0,0 +1,8 @@
+print "\nDigite o seu nome: "
+nome = gets.chomp
+print "Digite o seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp
+
+puts "\nSeu nome é #{nome} #{sobrenome} e você tem #{idade} anos"
